9edd5d88aa31604bc103dd9bef361ec34b816e64
sky <skybride@hotmail.com>
2018-08-02 14:56:15 -0400

    Adding Enemy Path

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class EnemyMovement : MonoBehaviour {
+
+	[SerializeField] List<Block> path;
+
+	void Start ()
+	{
+		PrintAllWaypoints();
+	}
+
+	private void PrintAllWaypoints()
+	{
+		foreach (Block waypoint in path) 
+		{
+			print (waypoint.name);
+		}
+	}
+}
