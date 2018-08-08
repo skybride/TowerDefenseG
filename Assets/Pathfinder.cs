74e6d1db1b4f2e759cac9d6c6eb05181ca336062
sky <skybride@hotmail.com>
2018-08-07 22:37:55 -0400

    Adding colors to start waypoint block and end waypoint block

---
+++ Assets/Pathfinder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour {
+
+	[SerializeField] Waypoint startWaypoint, endWaypoint;
+
+	Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+
+	// Use this for initialization
+	void Start () {
+		LoadBlocks ();
+		ColorStartAndEnd ();
+	}
+
+	private void ColorStartAndEnd ()
+	{
+		startWaypoint.SetTopColor (Color.green);
+		endWaypoint.SetTopColor (Color.red);
+	}
+
+	private void LoadBlocks()
+	{
+		var waypoints = FindObjectsOfType<Waypoint>();
+		foreach (Waypoint waypoint in waypoints) 
+		{
+			var gridPos = waypoint.GetGridPos ();
+			if (grid.ContainsKey(gridPos)) {
+				Debug.LogWarning ("Skipping overlapping block " + waypoint);
+			} 
+			else 
+			{
+				grid.Add(gridPos, waypoint);
+			}
+		}
+	}
+}
--- Assets/Waypoint.cs
+++ Assets/Waypoint.cs
@@ -13,11 +13,17 @@ public class Waypoint : MonoBehaviour {
 		return gridSize;
 	}
 
-	public Vector2 GetGridPos()
+	public Vector2Int GetGridPos()
 	{
 		return new Vector2Int(
 			Mathf.RoundToInt (transform.position.x / gridSize) * gridSize,
 			Mathf.RoundToInt (transform.position.z / gridSize) * gridSize
 		);
+	}
+
+	public void SetTopColor(Color color)
+	{
+		MeshRenderer topMeshRenderer = transform.Find ("Top").GetComponent<MeshRenderer>();
+		topMeshRenderer.material.color = color;
 	}
 }
