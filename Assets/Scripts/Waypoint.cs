bdd2514b662aa36762dbb00e7ea615d3de6ca17e
sky <skybride@hotmail.com>
2018-08-22 17:54:56 -0400

    trouble shooting when adding OnMouseOver() solution was collider

--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -30,4 +30,18 @@ public class Waypoint : MonoBehaviour {
 		MeshRenderer topMeshRenderer = transform.Find ("Top").GetComponent<MeshRenderer>();
 		topMeshRenderer.material.color = color;
 	}
+
+	void OnMouseOver()
+	{
+		//If your mouse hovers over the GameObject with the script attached, output this message
+		print(gameObject.name);
+	}
+
+	/*
+	void OnMouseExit()
+	{
+		//The mouse is no longer hovering over the GameObject so output this message each frame
+		Debug.Log("Mouse is no longer on GameObject.");
+	}
+	*/
 }
