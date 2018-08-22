1a908c7cf7feb4aaba68abc040574a50859e5148
sky <skybride@hotmail.com>
2018-08-22 17:01:30 -0400

    Coroutines Instantiations and setting Towers to target closest Enemy

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 using System;
 
 public class Tower : MonoBehaviour {
-
+	//Parameters
 	[SerializeField] Transform objectToPan;
-	[SerializeField] Transform targetEnemy;
 	[SerializeField] float attackRange = 10f;
 	[SerializeField] ParticleSystem projectileParticle;
+
+	//State of each tower
+	Transform targetEnemy;
 	
 	// Update is called once per frame
 	void Update () {
+		SetTargetEnemy ();
 		if (targetEnemy) {
 			objectToPan.LookAt (targetEnemy);
 			fireAtEnemy ();
@@ -20,9 +23,33 @@ public class Tower : MonoBehaviour {
 		{
 			Shoot (false);
 		}
+	}
 
-		//checkRange ();
+	private void SetTargetEnemy()
+	{
+		var sceneEnemies = FindObjectsOfType<EnemyDamage> ();
+		if (sceneEnemies.Length == 0) { return; } //guard condition
 
+		Transform closestEnemy = sceneEnemies[0].transform;
+
+		foreach (EnemyDamage testEnemy in sceneEnemies) 
+		{
+			closestEnemy = GetClosest (closestEnemy, testEnemy.transform);
+		}
+
+		targetEnemy = closestEnemy;
+	}
+
+	private Transform GetClosest(Transform transformA, Transform transformB)
+	{
+		var distToA = Vector3.Distance (transform.position, transformA.position);
+		var distToB = Vector3.Distance (transform.position, transformB.position);
+
+		if (distToA < distToB) 
+		{
+			return transformA;
+		}
+		return transformB;
 	}
 
 	/* tired myself
